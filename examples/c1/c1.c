c1a5c733511345cd4859bd5383df657c2c6f25cb
Arseniy Khvorov <khvorov45@gmail.com>
2021-10-20 11:50:49 +1100

    simulate data

--- examples/c1/c1.c
+++ examples/c1/c1.c
@@ -4,39 +4,73 @@
 
 int
 main() {
+    pcg64_random_t rng;
+    pcg_setseq_128_srandom_r(&rng, 1, 0);
+
     SmlConstants constants = sml_default_constants();
 
-    SmlInput input;
-    input.n_individuals = 1;
-    input.data = malloc(input.n_individuals * sizeof(SmlInputIndividual));
-    for (uint64_t individual_index = 0; individual_index < input.n_individuals; individual_index++) {
-        SmlInputIndividual* individual = input.data + individual_index;
-        individual->event_count = 1;
-        individual->events = malloc(individual->event_count * sizeof(SmlInputEvent));
-        for (uint64_t event_index = 0; event_index < individual->event_count; event_index++) {
-            SmlInputEvent* event = individual->events + event_index;
-            event->type = SmlEvent_Vaccination;
-            event->time = 0;
-        }
-        individual->titre_count = 3;
-        individual->titres = malloc(individual->titre_count * sizeof(SmlInputTitre));
-        double titre_times[3] = { 0, 14, 200 };
-        for (uint64_t titre_index = 0;
-            titre_index < individual->titre_count;
-            titre_index++) {
-            SmlInputTitre* titre = individual->titres + titre_index;
-            titre->log2titre = constants.lowest_log2titre;
-            titre->time = titre_times[titre_index];
-        }
-    }
+    SmlParameters pars_true = {
+        .vaccination_log2diff = 2,
+        .baseline = constants.lowest_log2titre,
+        .baseline_sd = 0,
+        .wane_rate = 0,
+        .residual_sd = 0.2,
+    };
 
     SmlParameters pars_init = {
         .vaccination_log2diff = 2,
         .baseline = constants.lowest_log2titre,
-        .baseline_sd = 1,
+        .baseline_sd = 0,
         .wane_rate = 0,
-        .residual_sd = 1,
+        .residual_sd = 0.2,
     };
+
+    SmlInput input;
+    input.n_individuals = 1000;
+    input.data = malloc(input.n_individuals * sizeof(SmlInputIndividual));
+
+    for (uint64_t individual_index = 0;
+        individual_index < input.n_individuals;
+        individual_index++) {
+
+        SmlInputIndividual* individual = input.data + individual_index;
+        individual->event_count = 1;
+        individual->events = malloc(individual->event_count * sizeof(SmlInputEvent));
+
+        for (uint64_t event_index = 0;
+            event_index < individual->event_count;
+            event_index++) {
+
+            SmlInputEvent* event = individual->events + event_index;
+            event->type = SmlEvent_Vaccination;
+            event->time = 0;
+        }
+
+        individual->titre_count = 3;
+        individual->titres = malloc(individual->titre_count * sizeof(SmlInputTitre));
+        double titre_times[3] = { 0, 14, 200 };
+
+        for (uint64_t titre_index = 0;
+            titre_index < individual->titre_count;
+            titre_index++) {
+
+            SmlInputTitre* titre = individual->titres + titre_index;
+
+            titre->time = titre_times[titre_index];
+
+            double time_since = titre->time; // NOTE(sen) Only one event at time 0
+            double up_slope = pars_true.vaccination_log2diff / constants.time_to_peak;
+            double past_peak = (double)(time_since > constants.time_to_peak);
+
+            double log2titre_expected = pars_true.baseline +
+                up_slope * time_since -
+                past_peak * (up_slope + pars_true.wane_rate) *
+                (time_since - constants.time_to_peak);
+
+            titre->log2titre =
+                sml_rnorm01(&rng) * pars_true.residual_sd + log2titre_expected;
+        }
+    }
 
     SmlOutput output;
     output.n_iterations = 10000;
